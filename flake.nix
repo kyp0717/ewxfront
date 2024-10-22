167077da843878a9cb3b9a2ef26f51cc6ffe48ff
Phage Ky <kyp0717@gmail.com>
2024-10-22 18:03:56 -0400

    add templ 3

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Elyth's NeoVim configuration";
 
   inputs = {
-    templ.url = "github:a-h/templ"; 
+    templ.url = "github:a-h/templ";
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
     nixvim = {
@@ -36,19 +36,19 @@
             inherit pkgs;
             module = ./config;
           };
-	  templOverlay = templ.packages.${system}.templ;
+          templOverlay = templ.packages.${system}.templ;
         in
         {
 
           packages = {
-            default = [templOverlay nvim];
+            default = [ templOverlay nvim ];
           };
 
           devShells = {
             default = with pkgs;
               mkShell {
                 packages = [
-		  templOverlay
+                  templOverlay
                   nvim
                   nodejs
                 ];
