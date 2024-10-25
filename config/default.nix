6c169cae46bb3d7137a410bd342542248a9e2e93
Phage Ky <kyp0717@gmail.com>
2024-10-24 20:05:41 -0400

    disable copilot

--- config/default.nix
+++ config/default.nix
@@ -71,11 +71,11 @@
       default = lib.mkDefault "tokyonight";
       type = lib.types.enum [ "paradise" "decay" "edge-dark" "mountain" "tokyonight" "everforest" "everblush" "jellybeans" "aquarium" "gruvbox" ];
     };
-    assistant = lib.mkOption {
-      default = "none";
-      # type = lib.types.enum [ "copilot" "none" ];
-      type = lib.types.enum [ "none" ];
-    };
+    # assistant = lib.mkOption {
+    # default = "none";
+    # type = lib.types.enum [ "copilot" "none" ];
+    # type = lib.types.enum [ "none" ];
+    # };
   };
   config = {
     # The base16 theme to use, if you want to use another theme, change it in colorscheme.nix
